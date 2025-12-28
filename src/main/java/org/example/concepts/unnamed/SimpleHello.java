d485ed0693055f7ee14d66106958ed0402d0b368
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2025-12-28 14:30:15 +0530

    Added samples

---
+++ src/main/java/org/example/concepts/unnamed/SimpleHello.java
@@ -0,0 +1,11 @@
+/**
+ * The Simplest Java Program - From Blog
+ * 
+ * Three lines. No class declaration. No public static.
+ * 
+ * Run: java --enable-preview --source 21 SimpleHello.java
+ */
+
+void main() {
+    System.out.println("Hello World!");
+}
